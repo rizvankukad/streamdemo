7a42600d9fda55db8f102493c3fb69accc151a71
Rizwan Kukad <rizvankukad@gmail.com>
2015-01-02 00:13:29 +0530

    Content length added
    Content length added

--- app.js
+++ app.js
@@ -28,9 +28,10 @@ app.use('/users', users);
 app.get('/streamaudio', function(req, res) {
 
     //res.writeHead(200,{ 'Content-Type' : 'audio/mpeg'});
-
+    var stats = fs.statSync("../music/test.mp3")
+ var fileSizeInBytes = stats["size"];
     var readableStream = fs.createReadStream("../music/test.mp3");
-    res.status(200).set({ 'Content-Type' : 'audio/mpeg'});
+    res.status(200).set({ 'Content-Type' : 'audio/mpeg', 'content-length': fileSizeInBytes});
     readableStream.pipe(res);
 
     readableStream.on('data', function(chunk) {
