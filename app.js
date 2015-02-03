f01487c85ae44c0c2c6ba93eb022cddc170a5f20
Rizwan Kukad <rizvankukad@gmail.com>
2015-02-03 09:43:41 +0530

    changes
    changes

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ app.use('/users', users);
 
 app.get('/streamaudio', function(req, res) {
 
-    var stats = fs.statSync("../music/test.mp3")
+    var stats = fs.statSync("./music/test.mp3");
     var fileSizeInBytes = stats["size"];
-    var readableStream = fs.createReadStream("../music/test.mp3");
+    var readableStream = fs.createReadStream("./music/test.mp3");
     res.status(200).set({ 'Content-Type' : 'audio/mpeg', 'content-length': fileSizeInBytes});
     readableStream.pipe(res);
 
